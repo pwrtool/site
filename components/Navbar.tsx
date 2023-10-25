78db46f8320bade9f13aaf46ca6b03aef65b01fc
firesquid <jdeiss06@gmail.com>
2023-10-24 22:32:16 -0500

    add icons to header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import { FaGithub, FaBook } from "react-icons/fa";
+import { RiInstallLine } from "react-icons/ri";
 
 export default function Navbar() {
   return (
-    <header className="p-4 border-bottom">
+    <header className="p-4 border-bottom flex flex-row content-center">
       <Link
         href="/"
         className="text-3xl font-bold text-white underline-hover hover:text-white"
@@ -10,9 +12,39 @@ export default function Navbar() {
         Powertool
       </Link>
 
-      <nav></nav>
+      <span className="flex-grow" />
+
+      <nav className="flex flex-row space-between">
+        {navItems.map((item) => (
+          <NavItem key={item.href} {...item} />
+        ))}
+      </nav>
     </header>
   );
 }
 
-function NavItem() {}
+const navItems: NavItemProps[] = [
+  { text: "Docs", href: "/about", icon: <FaBook /> },
+  { text: "Install", href: "/contact", icon: <RiInstallLine /> },
+  { text: "GitHub", href: "/github", icon: <FaGithub /> },
+];
+
+interface NavItemProps {
+  text: string;
+  href: string;
+  icon?: React.ReactNode;
+}
+
+function NavItem({ text, href, icon }: NavItemProps) {
+  return (
+    <div className="text-2xl mx-4 w-24">
+      <Link
+        href={href}
+        className="text-white underline-hover hover:text-white flex flex-row content-center justify-center"
+      >
+        <span className="m-1 ">{icon}</span>
+        {text}
+      </Link>
+    </div>
+  );
+}
