e517b78147b597bbfd61ecc0f1f67e99400f5093
firesquid <jdeiss06@gmail.com>
2023-11-09 19:36:28 -0600

    fix MDX

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,14 @@
 import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(): MDXComponents {
+  return getMDXComponents();
+}
+
+export function getIdFromChildren(children: React.ReactNode): string {
+  return (children || "").toString().toLowerCase().replace(/ /g, "-");
+}
+
+function getMDXComponents(): MDXComponents {
   return {
     h1: (props) => <h1 id={getIdFromChildren(props.children)} {...props} />,
     h2: (props) => <h2 id={getIdFromChildren(props.children)} {...props} />,
@@ -8,8 +16,4 @@ export function useMDXComponents(): MDXComponents {
     h4: (props) => <h4 id={getIdFromChildren(props.children)} {...props} />,
     p: (props) => <p {...props} />,
   };
-}
-
-export function getIdFromChildren(children: React.ReactNode): string {
-  return (children || "").toString().toLowerCase().replace(/ /g, "-");
 }
