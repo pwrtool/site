109d6cec28c9462c33c3e11d45380ca30641bccc
firesquid <jdeiss06@gmail.com>
2023-11-11 10:54:22 -0600

    add shadow around the big logo

--- components/BigLogo.tsx
+++ components/BigLogo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function BigLogo() {
   return (
-    <div className="mx-auto big-logo-shadow p-12 m-12 text-center">
+    <div className="mx-auto p-12 m-12 text-center">
       <Image
         src="/branding/colored_icon.svg"
         alt="Powertool Logo"
@@ -10,6 +10,7 @@ export default function BigLogo() {
         width={200}
         height={200}
       />
+      <div className="big-logo-shadow" />
       <h1 className="text-6xl font-bold text-center">Powertool</h1>
     </div>
   );
